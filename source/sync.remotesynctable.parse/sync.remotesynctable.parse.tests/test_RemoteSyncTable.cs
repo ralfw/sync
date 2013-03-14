924f63d318a6bb5ef53f19bdc6a2e6a74b82861b
Ralf Westphal <info@ralfw.de>
2013-03-14 13:29:43 +0100

    RemoteSyncTable implemented for parse.com

---
+++ source/sync.remotesynctable.parse/sync.remotesynctable.parse.tests/test_RemoteSyncTable.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using sync.contracts;
+
+namespace sync.remotesynctable.parse.tests
+{
+    [TestFixture]
+    public class test_RemoteSyncTable
+    {
+        private RemoteSyncTable _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            using (var sr = new StreamReader(@"..\..\..\..\..\unversioned\.syncconfig"))
+            {
+                var appId = sr.ReadLine();
+                var restKey = sr.ReadLine();
+
+                _sut = new RemoteSyncTable("testrepo", appId, restKey);
+            }
+        }
+
+
+        [Test, Explicit]
+        public void Add_entry()
+        {
+            _sut.AddEntry(new RepoFile{Id="myid" + DateTime.Now.ToString("s"),
+                                       RelativeFileName = "myfn",
+                                       RepoRoot = "myroot",
+                                       TimeStamp = DateTime.Now,
+                                       User="myuser"});
+        }
+
+
+        [Test, Explicit]
+        public void Update_entry()
+        {
+            var repoFile = new RepoFile{Id="myid" + DateTime.Now.ToString("s"),
+                                       RelativeFileName = "myfn",
+                                       RepoRoot = "myroot",
+                                       TimeStamp = DateTime.Now,
+                                       User="myuser"};
+            _sut.AddEntry(repoFile);
+
+            repoFile.Id = "myid new " + DateTime.Now.ToString("s");
+            repoFile.User = "myuser new";
+
+            RepoFile result = null;
+            _sut.UpdateEntry(repoFile, _ => result = _, null); 
+  
+            Assert.AreEqual(repoFile.Id, result.Id);
+        }
+
+        [Test, Explicit]
+        public void Unknown_entry_to_update()
+        {
+            var repoFile = new RepoFile
+            {
+                Id = "myid" + DateTime.Now.ToString("s"),
+                RelativeFileName = "my unknown fn",
+                RepoRoot = "myroot",
+                TimeStamp = DateTime.Now,
+                User = "myuser"
+            };
+
+            RepoFile result = null;
+            _sut.UpdateEntry(repoFile, null, _ => result = _);
+
+            Assert.AreEqual(repoFile.Id, result.Id);
+        }
+
+        [Test, Explicit]
+        public void Delete_entry()
+        {
+            var repoFile = new RepoFile {
+                                            Id = "myid" + DateTime.Now.ToString("s"),
+                                            RelativeFileName = "myfntobedeleted",
+                                            RepoRoot = "myroot",
+                                            TimeStamp = DateTime.Now,
+                                            User = "myuser"
+                                        };
+            _sut.AddEntry(repoFile);
+
+            var result = _sut.DeleteEntry(repoFile);
+
+            Assert.AreEqual(repoFile.Id, result.Id);
+        }
+
+        [Test, Explicit]
+        public void Delete_nonexisting_entry()
+        {
+            var repoFile = new RepoFile
+            {
+                Id = "myid" + DateTime.Now.ToString("s"),
+                RelativeFileName = "my nonexisting entry",
+                RepoRoot = "myroot",
+                TimeStamp = DateTime.Now,
+                User = "myuser"
+            };
+
+            var result = _sut.DeleteEntry(repoFile);
+
+            Assert.AreEqual(repoFile.Id, result.Id);
+        }
+
+        [Test, Explicit]
+        public void Test()
+        {
+            _sut.AddEntry(new RepoFile  {
+                                            Id = "myid" + DateTime.Now.ToString("s"),
+                                            RelativeFileName = "myfn",
+                                            RepoRoot = "myroot",
+                                            TimeStamp = DateTime.Now,
+                                            User = "myuser"
+                                        });
+
+            var results = new List<RepoFile>();
+
+            _sut.CollectRepoFiles(results.Add);
+
+            Assert.IsTrue(results.Count > 0);
+        }
+
+        [Test]
+        public void Filter_existing_file()
+        {
+            _sut.AddEntry(new RepoFile  {
+                                            Id = "myid" + DateTime.Now.ToString("s"),
+                                            RelativeFileName = "myfn",
+                                            RepoRoot = "myroot",
+                                            TimeStamp = DateTime.Now,
+                                            User = "myuser"
+                                        });
+
+            RepoFile result = null;
+            _sut.FilterExistingFiles(new RepoFile{RelativeFileName = "myfn"}, _ => result = _);
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void Pass_on_nonexisting_file()
+        {
+            RepoFile result = null;
+            _sut.FilterExistingFiles(new RepoFile { RelativeFileName = "my nonexisting fn" }, _ => result = _);
+
+            Assert.AreEqual("my nonexisting fn", result.RelativeFileName);
+        }
+    }
+}
--- source/sync.remotesynctable.parse/sync.remotesynctable.parse/RemoteSyncTable.cs
+++ source/sync.remotesynctable.parse/sync.remotesynctable.parse/RemoteSyncTable.cs
@@ -3,40 +3,122 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using sync.contracts;
+using sync.remotefilestore.parse.api;
 
 namespace sync.remotesynctable.parse
 {
     public class RemoteSyncTable : IRemoteSyncTable
     {
-        public RemoteSyncTable(string repoName)
+        private readonly string _repoName;
+        private readonly ParseObjects _parseObjects;
+        private JavaScriptSerializer _jss;
+
+        public RemoteSyncTable(string repoName, string parseAppId, string parseRestKey)
         {
-            
+            _repoName = repoName;
+            _parseObjects = new ParseObjects(parseAppId, parseRestKey);
+
+            _jss = new JavaScriptSerializer();
         }
 
-        public void CollectRepoFiles(Action<RepoFile> continueWith)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateEntry(RepoFile repoFile, Action<RepoFile> onEntryUpdated, Action<RepoFile> onNoEntry)
-        {
-            throw new NotImplementedException();
-        }
 
         public void AddEntry(RepoFile repoFile)
         {
-            throw new NotImplementedException();
+            var jsonRepoFile = repoFile.ToJson();
+            Console.WriteLine(jsonRepoFile);
+            _parseObjects.New(_repoName, jsonRepoFile);
         }
+
+
+        public void UpdateEntry(RepoFile repoFile, Action<RepoFile> onEntryUpdated, Action<RepoFile> onNoEntry)
+        {
+            Dictionary<string, object> item;
+            if (TryFindEntry(repoFile, out item))
+            {
+                _parseObjects[_repoName, item["objectId"].ToString()] = repoFile.ToJson();
+
+                onEntryUpdated(repoFile);
+            }
+            else
+                onNoEntry(repoFile);
+        }
+
 
         public RepoFile DeleteEntry(RepoFile repoFile)
         {
-            throw new NotImplementedException();
+            Dictionary<string, object> item;
+            if (TryFindEntry(repoFile, out item))
+            {
+                _parseObjects.Delete(_repoName, item["objectId"].ToString());   
+            }
+            return repoFile;
         }
+
+
+        public void CollectRepoFiles(Action<RepoFile> continueWith)
+        {
+            var jsonQueryResults = _parseObjects.Query(_repoName);
+
+            var queryResults = (Dictionary<string, object>)_jss.DeserializeObject(jsonQueryResults);
+            var queryResultItems = (object[])queryResults["results"];
+
+            foreach(var item in queryResultItems)
+            {
+                var repoFile = ((Dictionary<string, object>)item).ToRepoFile();
+                continueWith(repoFile);
+            }
+        }
+
 
         public void FilterExistingFiles(RepoFile repoFile, Action<RepoFile> onNonExistingFile)
         {
-            throw new NotImplementedException();
+            Dictionary<string, object> _;
+            if (!TryFindEntry(repoFile, out _))
+                onNonExistingFile(repoFile);
+        }
+
+
+        bool TryFindEntry(RepoFile repoFile, out Dictionary<string, object> dictRepoFile)
+        {
+            var jsonQueryResults = _parseObjects.Query(_repoName, "{\"relativeFilename\":\"" + repoFile.RelativeFileName + "\"}");
+
+            var queryResults = (Dictionary<string,object>)_jss.DeserializeObject(jsonQueryResults);
+            var queryResultItems = (object[])queryResults["results"];
+
+            if (queryResultItems.Length > 0)
+                dictRepoFile = (Dictionary<string, object>) queryResultItems[0];
+            else
+                dictRepoFile = null;
+
+            return dictRepoFile != null;
+        }
+    }
+
+
+    static class RepoFileSerializer
+    {
+        public static string ToJson(this RepoFile repoFile)
+        {
+            return "{" + 
+                        string.Format("\"relativeFilename\": \"{0}\",\n", repoFile.RelativeFileName) +
+                        string.Format("\"idInFilestore\": \"{0}\",\n", repoFile.Id) +
+                        string.Format("\"timeStamp\": \"{0}\",\n", repoFile.TimeStamp.ToString("s")) +
+                        string.Format("\"user\": \"{0}\"", repoFile.User) +
+                   "}";
+
+        }
+
+        public static RepoFile ToRepoFile(this Dictionary<string, object> dictRepoFile)
+        {
+            return new RepoFile
+                {
+                    Id = dictRepoFile["idInFilestore"].ToString(),
+                    RelativeFileName = dictRepoFile["relativeFilename"].ToString(),
+                    TimeStamp = DateTime.Parse(dictRepoFile["timeStamp"].ToString()),
+                    User = dictRepoFile["user"].ToString()
+                };
         }
     }
 }
